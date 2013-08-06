3848e34db9ac587f1603e4640dd1ed3875acaeec
Michael Fink <michael.fink@asamnet.de>
2013-08-06 21:52:34 +0200

    added project ulib.Unit

---
+++ src/ulib/ulib.Unit/stdafx.h
@@ -0,0 +1,20 @@
+//
+// ulib - a collection of useful classes
+// Copyright (C) 2012 Michael Fink
+//
+/// \file ulib.Unit\stdafx.h precompiled header support
+//
+#pragma once
+
+// including this defines the highest available windows platform
+#if _MSC_VER <= 1500
+#include <SDKDDKVer.h>
+#else
+// to build for a previous version, use this include and define
+#include <WinSDKVer.h>
+#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#include <SDKDDKVer.h>
+#endif
+
+#include <ulib/config/Atl.hpp>
+#include "CppUnitTest.h"
