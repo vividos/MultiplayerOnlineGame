d6d0fa1c16ff1ff8bb3151f207b54aabc1696466
Michael Fink <michael.fink@asamnet.de>
2013-09-27 16:15:47 +0200

    added Android project files for Underworld project

---
+++ src/Underworld/AndroidApk/jni/src/Main.cpp
@@ -0,0 +1,16 @@
+//
+// Underworld - an Ultima Underworld remake project
+// Copyright (C) 2002-2013 Michael Fink
+//
+/// \file Underworld/AndroidApk/jni/src/Main.cpp Android app main function
+//
+
+// includes
+#include <ulib/config/Android.hpp>
+#include <SDL.h>
+
+int main(int argc, char *argv[])
+{
+   // TODO implement
+   return 0;
+}
---
+++ src/Underworld/AndroidApk/src/com/vividos/underworld/UnderworldActivity.java
@@ -0,0 +1,9 @@
+package com.vividos.underworld;
+
+import org.libsdl.app.SDLActivity; 
+
+/* 
+ * A sample wrapper class that just calls SDLActivity 
+ */ 
+
+public class UnderworldActivity extends SDLActivity { }
