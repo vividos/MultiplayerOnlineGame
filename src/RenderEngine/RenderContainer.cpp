5e8eb8fe984af1d102782723217b15e707505d2a
Michael Fink <michael.fink@asamnet.de>
2013-10-24 22:32:33 +0200

    added class RenderContainer

---
+++ src/RenderEngine/RenderContainer.cpp
@@ -0,0 +1,103 @@
+//
+// MultiplayerOnlineGame - multiplayer game project
+// Copyright (C) 2008-2013 Michael Fink
+//
+//! \file RenderContainer.cpp Render container
+//
+
+// includes
+#include "StdAfx.h"
+#include "RenderContainer.hpp"
+#include "IRenderable.hpp"
+#include "GraphicsTaskManager.hpp"
+#include <boost/foreach.hpp>
+
+RenderContainer::RenderContainer(GraphicsTaskManager& taskManager)
+:m_taskManager(taskManager)
+{
+}
+
+void RenderContainer::Add(std::shared_ptr<IRenderable> spRenderObject)
+{
+   if (spRenderObject->IsPrepareNeeded())
+   {
+      m_taskManager.BackgroundTaskGroup().Add(
+         boost::bind(&RenderContainer::AsyncPrepare, this, spRenderObject));
+   }
+   else
+   if (spRenderObject->IsUploadNeeded())
+   {
+      // no prepare, but update
+      m_taskManager.UploadTaskGroup().Add(
+         boost::bind(&RenderContainer::AsyncUpload, this, spRenderObject));
+   }
+   else
+   {
+      // no prepare, no update: just add
+      m_taskManager.UploadTaskGroup().Add(
+         boost::bind(&RenderContainer::InternalAdd, this, spRenderObject));
+   }
+}
+
+void RenderContainer::Remove(std::shared_ptr<IRenderable> spRenderObject)
+{
+   // no prepare, no update: just add
+   m_taskManager.UploadTaskGroup().Add(
+      boost::bind(&RenderContainer::InternalRemove, this, spRenderObject));
+}
+
+void RenderContainer::Render(RenderOptions& options)
+{
+   BOOST_FOREACH(std::shared_ptr<IRenderable> spRenderObject, m_vecRenderObjects)
+      spRenderObject->Render(options);
+}
+
+void RenderContainer::AsyncPrepare(std::shared_ptr<IRenderable> spRenderObject)
+{
+   ATLASSERT(spRenderObject->IsPrepareNeeded());
+
+   spRenderObject->Prepare();
+
+   if (spRenderObject->IsUploadNeeded())
+   {
+      m_taskManager.UploadTaskGroup().Add(
+         boost::bind(&RenderContainer::AsyncUpload, this, spRenderObject));
+   }
+   else
+   {
+      // no update: just add (in upload task)
+      m_taskManager.UploadTaskGroup().Add(
+         boost::bind(&RenderContainer::InternalAdd, this, spRenderObject));
+   }
+}
+
+void RenderContainer::AsyncUpload(std::shared_ptr<IRenderable> spRenderObject)
+{
+   ATLASSERT(spRenderObject->IsPrepareNeeded());
+
+   spRenderObject->Upload();
+
+   // since we're already in the upload task, add object right away
+   InternalAdd(spRenderObject);
+}
+
+void RenderContainer::InternalAdd(std::shared_ptr<IRenderable> spRenderObject)
+{
+   m_vecRenderObjects.push_back(spRenderObject);
+}
+
+void RenderContainer::InternalRemove(std::shared_ptr<IRenderable> spRenderObject)
+{
+   spRenderObject->Done();
+
+   for (size_t i=0, iMax=m_vecRenderObjects.size(); i<iMax; i++)
+   {
+      if (spRenderObject == m_vecRenderObjects[i])
+      {
+         m_vecRenderObjects.erase(m_vecRenderObjects.begin() + i);
+         return;
+      }
+   }
+
+   ATLASSERT(false); // object should be in container!
+}
---
+++ src/RenderEngine/RenderContainer.hpp
@@ -0,0 +1,53 @@
+//
+// MultiplayerOnlineGame - multiplayer game project
+// Copyright (C) 2008-2013 Michael Fink
+//
+//! \file RenderContainer.hpp Render container
+//
+#pragma once
+
+// includes
+#include "RenderEngineCommon.hpp"
+#include <vector>
+
+// forward references
+class GraphicsTaskManager;
+class IRenderable;
+class RenderOptions;
+
+/// \brief container for renderable objects
+class RENDERENGINE_DECLSPEC RenderContainer
+{
+public:
+   /// ctor
+   RenderContainer(GraphicsTaskManager& taskManager);
+
+   /// add renderable object
+   void Add(std::shared_ptr<IRenderable> spRenderObject);
+
+   /// remove renderable object
+   void Remove(std::shared_ptr<IRenderable> spRenderObject);
+
+   /// render all objects
+   void Render(RenderOptions& options);
+
+private:
+   /// prepare object in background thread
+   void AsyncPrepare(std::shared_ptr<IRenderable> spRenderObject);
+
+   /// upload object in OpenGL thread
+   void AsyncUpload(std::shared_ptr<IRenderable> spRenderObject);
+
+   /// internal add function
+   void InternalAdd(std::shared_ptr<IRenderable> spRenderObject);
+
+   /// internal remove function
+   void InternalRemove(std::shared_ptr<IRenderable> spRenderObject);
+
+private:
+   /// task manager
+   GraphicsTaskManager& m_taskManager;
+
+   /// all render objects
+   std::vector<std::shared_ptr<IRenderable>> m_vecRenderObjects;
+};
