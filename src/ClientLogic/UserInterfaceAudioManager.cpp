6481ba94223e674418a5975c164408f75cfac3ab
Michael Fink <michael.fink@asamnet.de>
2013-11-18 20:21:56 +0100

    added class UserInterfaceAudioManager

---
+++ src/ClientLogic/UserInterfaceAudioManager.cpp
@@ -0,0 +1,74 @@
+//
+// MultiplayerOnlineGame - multiplayer game project
+// Copyright (C) 2008-2013 Michael Fink
+//
+//! \file UserInterfaceAudioManager.cpp UI audio manager
+//
+
+// includes
+#include "StdAfx.h"
+#include "UserInterfaceAudioManager.hpp"
+#include "IAudioManager.hpp"
+#include "IWindowManager.hpp"
+#include "AudioSoundType.hpp"
+
+UserInterfaceAudioManager::UserInterfaceAudioManager(Audio::IAudioManager& audioManager)
+:m_audioManager(audioManager)
+{
+   LoadUserInterfaceSounds();
+}
+
+void UserInterfaceAudioManager::Connect(IWindowManager& windowManager)
+{
+   windowManager.SetAudioEventHandler(
+      std::bind(&UserInterfaceAudioManager::OnUserInterfaceAudioEvent, this, std::placeholders::_1));
+}
+
+void UserInterfaceAudioManager::OnUserInterfaceAudioEvent(T_enUserInterfaceAudioEvents enAudioEvent) throw()
+{
+   switch (enAudioEvent)
+   {
+   case uiButtonPress:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::ButtonPress);
+      break;
+   case uiButtonLeave:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::ButtonPress);
+      break;
+   case uiItemSelect:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::ItemSelected);
+      break;
+
+   case uiDialogOpenModal:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::Open);
+      break;
+
+   case uiDialogOpenNonmodal:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::Open);
+      break;
+
+   case uiDialogCloseModal:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::Close);
+      break;
+
+   case uiDialogCloseNonmodal:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::Close);
+      break;
+
+   case uiTooltipOpened:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::Open);
+      break;
+
+   case uiScrollOpen:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::Open);
+      break;
+
+   case uiScrollClosed:
+      m_audioManager.PlaySound(true, Audio::Sound::UI::Close);
+      break;
+   }
+}
+
+void UserInterfaceAudioManager::LoadUserInterfaceSounds()
+{
+   // TODO implement
+}
---
+++ src/ClientLogic/UserInterfaceAudioManager.hpp
@@ -0,0 +1,41 @@
+//
+// MultiplayerOnlineGame - multiplayer game project
+// Copyright (C) 2008-2013 Michael Fink
+//
+//! \file UserInterfaceAudioManager.hpp UI audio manager
+//
+#pragma once
+
+// includes
+#include "ClientLogic.hpp"
+#include "UserInterfaceAudio.hpp"
+
+/// forward references
+namespace Audio
+{
+class IAudioManager;
+}
+class IWindowManager;
+
+/// manages audio for client
+class CLIENTLOGIC_DECLSPEC UserInterfaceAudioManager: public boost::noncopyable
+{
+public:
+   /// ctor
+   UserInterfaceAudioManager(Audio::IAudioManager& audioManager);
+   /// dtor
+   ~UserInterfaceAudioManager() throw() {}
+
+   /// connects window manager to audio manager
+   void Connect(IWindowManager& windowManager);
+
+   // called on user interface audio event
+   void OnUserInterfaceAudioEvent(T_enUserInterfaceAudioEvents enAudioEvent) throw();
+
+private:
+   void LoadUserInterfaceSounds();
+
+private:
+   /// audio manager
+   Audio::IAudioManager& m_audioManager;
+};
