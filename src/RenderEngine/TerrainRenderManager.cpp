002a75740ff24d4bd509dba5577f9712d34f775b
Michael Fink <michael.fink@asamnet.de>
2014-01-14 18:36:50 +0100

    added dummy class TerrainRenderManager

---
+++ src/RenderEngine/TerrainRenderManager.cpp
@@ -0,0 +1,27 @@
+//
+// MultiplayerOnlineGame - multiplayer game project
+// Copyright (C) 2008-2014 Michael Fink
+//
+//! \file TerrainRenderManager.cpp Terrain render manager
+//
+#pragma once
+
+// includes
+#include "stdafx.h"
+#include "TerrainRenderManager.hpp"
+
+TerrainRenderManager::TerrainRenderManager(IFileSystem& /*fileSystem*/, GraphicsTaskManager& /*taskManager*/)
+{
+}
+
+void TerrainRenderManager::SetPosition(const Vector3d& /*vPos*/, double /*dAngleDir*/)
+{
+}
+
+void TerrainRenderManager::Render(RenderOptions& /*options*/)
+{
+}
+
+void TerrainRenderManager::Done()
+{
+}
