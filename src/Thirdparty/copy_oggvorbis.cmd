f40d6673b00133eb0c53411b386484ee9c631f1d
Michael Fink <michael.fink@asamnet.de>
2014-03-16 12:38:57 +0100

    changed copy_oggvorbis.cmd to preserve vc110.pdb files

--- src/Thirdparty/copy_oggvorbis.cmd
+++ src/Thirdparty/copy_oggvorbis.cmd
@@ -8,9 +8,9 @@ mkdir oggvorbis\lib\Release\Win32
 mkdir oggvorbis\lib\Release\x64
 
 for %%P in (Win32 x64) do for %%C in (Debug Release) do copy libogg\win32\VS2012\%%P\%%C\libogg_static.lib oggvorbis\lib\%%C\%%P\
-for %%P in (Win32 x64) do for %%C in (Debug Release) do copy libogg\win32\VS2012\%%P\%%C\vc110.pdb         oggvorbis\lib\%%C\%%P\libogg_static.pdb
+for %%P in (Win32 x64) do for %%C in (Debug Release) do copy libogg\win32\VS2012\%%P\%%C\vc110.pdb         oggvorbis\lib\%%C\%%P\
 
 for %%F in (vorbis vorbisfile) do for %%P in (Win32 x64) do for %%C in (Debug Release) do copy libvorbis\win32\VS2012\%%P\%%C\lib%%F_static.lib oggvorbis\lib\%%C\%%P\
-for %%F in (vorbis vorbisfile) do for %%P in (Win32 x64) do for %%C in (Debug Release) do copy libvorbis\win32\VS2012\lib%%F\%%P\%%C\vc110.pdb  oggvorbis\lib\%%C\%%P\lib%%F_static.pdb
+for %%F in (vorbis vorbisfile) do for %%P in (Win32 x64) do for %%C in (Debug Release) do copy libvorbis\win32\VS2012\lib%%F\%%P\%%C\vc110.pdb  oggvorbis\lib\%%C\%%P\
 
 pause
