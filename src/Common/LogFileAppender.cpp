571e090c894c9df48ef1a0b9a0d62c8362a165d3
Michael Fink <michael.fink@asamnet.de>
2014-10-11 16:03:07 +0200

    added LogFileAppender class

---
+++ src/Common/LogFileAppender.cpp
@@ -0,0 +1,50 @@
+//
+// MultiplayerOnlineGame - multiplayer game project
+// Copyright (C) 2008-2014 Michael Fink
+//
+/// \file LogFileAppender.cpp Log file appender
+//
+
+// includes
+#include "StdAfx.h"
+#include "LogFileAppender.hpp"
+#include "Filesystem.hpp"
+#include "Path.hpp"
+#include <ulib/DateTime.hpp>
+
+LogFileAppender::LogFileAppender()
+{
+   CString cszLogFilename = CreateLogFileName();
+
+   FILE* fd = NULL;
+   _tfopen_s(&fd, cszLogFilename, _T("at")); // append to text file
+   if (fd == NULL)
+      throw Exception(_T("couldn't open log file: ") + cszLogFilename, __FILE__, __LINE__);
+
+   m_spLogFile.reset(fd, fclose);
+
+   fseek(fd, 0, SEEK_END); // seek to end
+}
+
+void LogFileAppender::DoAppend(const Log::LoggingEventPtr spLoggingEvent)
+{
+   ATLASSERT(Layout().get() != NULL);
+
+   CString cszOutput;
+   Layout()->Format(cszOutput, spLoggingEvent);
+
+   _ftprintf(m_spLogFile.get(), _T("%s\n"), cszOutput.GetString());
+
+   fflush(m_spLogFile.get());
+}
+
+CString LogFileAppender::CreateLogFileName() throw()
+{
+   CString cszFolderName = Path::Combine(Filesystem::UserFolder(), _T("log"));
+
+   if (!Path(cszFolderName).FolderExists())
+      CreateDirectory(cszFolderName, nullptr);
+
+   DateTime today = DateTime::Today();
+   return Path::Combine(cszFolderName, today.Format(_T("Log_%Y-%m-%d.txt")));
+}
---
+++ src/Common/LogFileAppender.hpp
@@ -0,0 +1,33 @@
+//
+// MultiplayerOnlineGame - multiplayer game project
+// Copyright (C) 2008-2014 Michael Fink
+//
+/// \file LogFileAppender.hpp Log file appender
+//
+#pragma once
+
+// includes
+#include "Common.hpp"
+#include <ulib/log/Appender.hpp>
+
+/// \brief appender that writes to log file
+/// \details log file is created with current date in name
+class COMMON_DECLSPEC LogFileAppender : public Log::Appender
+{
+public:
+   /// ctor
+   LogFileAppender();
+   /// dtor
+   virtual ~LogFileAppender() throw() {}
+
+   /// outputs logging event to log file
+   virtual void DoAppend(const Log::LoggingEventPtr spLoggingEvent);
+
+private:
+   /// returns log file name
+   static CString CreateLogFileName() throw();
+
+private:
+   /// log file
+   boost::shared_ptr<FILE> m_spLogFile;
+};
