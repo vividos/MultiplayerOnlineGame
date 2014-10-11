50c3dea02ae6421a43895d87b347b15ccd39717b
Michael Fink <michael.fink@asamnet.de>
2014-10-11 22:16:49 +0200

    added GameClient project

---
+++ src/GameClient/GameClient.cpp
@@ -0,0 +1,77 @@
+//
+// MultiplayerOnlineGame - multiplayer game project
+// Copyright (C) 2008-2014 Michael Fink
+//
+/// \file src/GameClient/GameClient.cpp Main function
+//
+
+// includes
+#include "stdafx.h"
+#include "CrashReporter.hpp"
+#include "GameClient.hpp"
+#include "Filesystem.hpp"
+#include "IAudioManager.hpp"
+#include <ulib/log/Appender.hpp>
+#include <ulib/log/Layout.hpp>
+#include "LogFileAppender.hpp"
+
+GameClient::GameClient()
+:GameClientBase(_T("Game Client")),
+m_uiAudioManager(Audio::IAudioManager::Get())
+{
+   SetupLogging();
+}
+
+void GameClient::SetupLogging()
+{
+   {
+      Log::LayoutPtr spTraceLayout(new Log::PatternLayout(_T("%F(%L): log [%p] %m")));
+
+      Log::AppenderPtr spTraceAppender(new Log::OutputDebugStringAppender);
+      spTraceAppender->Layout(spTraceLayout);
+
+      Log::Logger::GetRootLogger()->AddAppender(spTraceAppender);
+   }
+
+   {
+      Log::LayoutPtr spLogFileLayout(new Log::PatternLayout(_T("%d [%p] [%c] %m")));
+
+      Log::AppenderPtr spLogFileAppender(new LogFileAppender);
+      spLogFileAppender->Layout(spLogFileLayout);
+
+      Log::Logger::GetRootLogger()->AddAppender(spLogFileAppender);
+   }
+}
+
+void GameClient::Start()
+{
+   Init(800, 600, false);
+
+   // enable audio events
+   m_uiAudioManager.Connect(GetWindowManager());
+
+   //std::shared_ptr<MainGameScene> spMainGameScene(new MainGameScene(*this, *this, m_game));
+   //ChangeScene(spMainGameScene);
+
+   Run();
+}
+
+/// GameClient main function
+int WINAPI _tWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPTSTR /*lpstrCmdLine*/, int /*nCmdShow*/)
+{
+   CrashReporter::Init(Filesystem().UserFolder() + c_pszCrashdumpFoldername);
+
+   try
+   {
+      GameClient gc;
+      gc.Start();
+   }
+   catch (Exception& ex)
+   {
+      CString cszText;
+      cszText.Format(_T("Arena ended with exception: %s"), ex.Message());
+      MessageBox(GetActiveWindow(), cszText, _T("Arena"), MB_OK);
+   }
+
+   return 0;
+}
---
+++ src/GameClient/GameClient.hpp
@@ -0,0 +1,30 @@
+//
+// MultiplayerOnlineGame - multiplayer game project
+// Copyright (C) 2008-2014 Michael Fink
+//
+/// \file src/GameClient/GameClient.hpp Arena game client
+//
+#pragma once
+
+// includes
+#include "GameClientBase.hpp"
+#include "UserInterfaceAudioManager.hpp"
+
+/// game client
+class GameClient : public GameClientBase
+{
+public:
+   /// ctor
+   GameClient();
+
+   /// starts game client
+   void Start();
+
+private:
+   /// sets up logging
+   void SetupLogging();
+
+private:
+   /// UI audio manager
+   UserInterfaceAudioManager m_uiAudioManager;
+};
