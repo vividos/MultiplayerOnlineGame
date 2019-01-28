b4d179210cd89386aa375aeb318d1b7ba90ae8ad
Michael Fink <michael.fink@asamnet.de>
2019-01-28 07:20:40 +0100

    moved cmd scripts from Thirdparty project file to standalone .cmd files

---
+++ src/Thirdparty/Build.cmd
@@ -0,0 +1,11 @@
+REM
+REM MultiplayerOnlineGame - multiplayer game project
+REM Copyright (C) 2008-2019 Michael Fink
+REM
+REM Build.cmd - Thirdparty build script
+REM
+
+copy "%1\freetype-2.3.5-1\freetype6.dll" "%2"
+copy "%1\SDL2-2.0.3\lib\x86\SDL2.dll" "%2"
+copy "%1\zlib128\zlib1.dll" "%2"
+copy "%1\OpenAL-SDK\oalinst.exe" "%2"
---
+++ src/Thirdparty/Clean.cmd
@@ -0,0 +1,11 @@
+REM
+REM MultiplayerOnlineGame - multiplayer game project
+REM Copyright (C) 2008-2019 Michael Fink
+REM
+REM Clean.cmd - Thirdparty clean script
+REM
+
+del "%1\freetype6.dll" > nul
+del "%1\SDL2.dll" > nul
+del "%1\zlib1.dll" > nul
+del "%1\oalinst.exe" > nul
