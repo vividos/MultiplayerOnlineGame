4a23d781f5dc7a791798da40c532a488364d2a12
Michael Fink <michael.fink@asamnet.de>
2019-01-28 08:51:19 +0100

    replaced ulib project with NuGet package Vividos.UlibCpp.Static

--- src/ulib/stdafx.cpp
+++
@@ -1,9 +0,0 @@
-//
-// ulib - a collection of useful classes
-// Copyright (C) 2006-2014 Michael Fink
-//
-/// \file src\stdafx.cpp precompiled header support
-//
-
-// includes
-#include "stdafx.h"
--- src/ulib/ulib.Unit/stdafx.cpp
+++
@@ -1,9 +0,0 @@
-//
-// ulib - a collection of useful classes
-// Copyright (C) 2012 Michael Fink
-//
-/// \file ulib.Unit\stdafx.cpp precompiled header support
-//
-
-// includes
-#include "stdafx.h"
