d5253130da65a8d77e7266a627fb599b7835abaa
Michael Fink <michael.fink@asamnet.de>
2019-01-29 07:30:05 +0100

    updated build script to use Visual Studio 2017

--- Build.cmd
+++ Build.cmd
@@ -1,15 +1,15 @@
 REM
 REM MultiplayerOnlineGame - multiplayer game project
-REM Copyright (C) 2008-2014 Michael Fink
+REM Copyright (C) 2008-2019 Michael Fink
 REM
 REM Build.cmd - Build script
 REM
 
 REM Set up environment
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 
 REM Build BuildData tool
 msbuild data\tools\BuildData\BuildData.csproj /p:Configuration=Release /p:Platform="Any CPU"
 
 REM Build all C++ projects
-msbuild src\MultiplayerOnlineGame.sln /m:4 /p:Configuration=Release /target:Rebuild
+msbuild src\MultiplayerOnlineGame.sln /m /p:Configuration=Release /target:Rebuild
