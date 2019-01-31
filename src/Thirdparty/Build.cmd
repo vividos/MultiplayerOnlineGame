ef7c8eba2d8554e81e02f3754e60baa8130f87a1
Michael Fink <michael.fink@asamnet.de>
2019-01-31 07:44:58 +0100

    fixed copying SDL2.dll to bin folder

--- src/Thirdparty/Build.cmd
+++ src/Thirdparty/Build.cmd
@@ -6,6 +6,6 @@ REM Build.cmd - Thirdparty build script
 REM
 
 copy "%1\freetype-2.3.5-1\freetype6.dll" "%2"
-copy "%1\SDL2-2.0.3\lib\x86\SDL2.dll" "%2"
+copy "%1\SDL2-2.0.9\lib\x86\SDL2.dll" "%2"
 copy "%1\zlib128\zlib1.dll" "%2"
 copy "%1\OpenAL-SDK\oalinst.exe" "%2"
