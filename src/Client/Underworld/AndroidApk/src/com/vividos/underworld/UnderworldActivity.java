cc9b0963fa1ad80428efcfd4bcf4e8d45d22176e
Michael Fink <michael.fink@asamnet.de>
2019-10-25 23:51:29 +0200

    removed Underworld code that is already contained in https://github.com/vividos/UnderworldAdventures

--- src/Client/Underworld/AndroidApk/src/com/vividos/underworld/UnderworldActivity.java
+++
@@ -1,9 +0,0 @@
-package com.vividos.underworld;
-
-import org.libsdl.app.SDLActivity; 
-
-/* 
- * A sample wrapper class that just calls SDLActivity 
- */ 
-
-public class UnderworldActivity extends SDLActivity { }
--- src/Client/Underworld/stdafx.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Underworld - an Ultima Underworld remake project
-// Copyright (C) 2002-2014 Michael Fink
-//
-/// \file Underworld/stdafx.cpp Precompiled header support
-//
-
-// includes
-#include "stdafx.h"
